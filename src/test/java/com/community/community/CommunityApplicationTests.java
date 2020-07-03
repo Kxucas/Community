6292d4c7cced4a9fff73cba716aa0ed3725a5579
kx <wkx_ucas@163.com>
2020-07-03 14:57:04 +0800

    increase

--- src/test/java/com/community/community/CommunityApplicationTests.java
+++ src/test/java/com/community/community/CommunityApplicationTests.java
@@ -1,13 +1,17 @@
 package com.community.community;
 
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.shiro.authz.AuthorizationInfo;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+
+import javax.sql.DataSource;
 
 @SpringBootTest
 class CommunityApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
 
 }
